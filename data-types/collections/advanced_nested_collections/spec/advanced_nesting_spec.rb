3bdf4107cf8c5c8c0b50ef0de6bcf2665ae7a12a
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-04 16:24:24 +0530

    data-types advanced nested collections complete

--- data-types/collections/advanced_nested_collections/spec/advanced_nesting_spec.rb
+++ data-types/collections/advanced_nested_collections/spec/advanced_nesting_spec.rb
@@ -21,57 +21,146 @@ RSpec.describe 'Advanced Nested Collections' do
     expect(employees).to eq(expected)
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     # Find the ingredients for pancakes
-    pancake_ingredients = _____
-
+    pancake_ingredients =[]
+      stores.each do |key, value| 
+        value.each do |k, v| 
+          if k == :dishes
+              v.each do |k1, v1| 
+                k1.each do |k2, v2|
+                  if v2 == "Pancakes"
+                  pancake_ingredients <<  k1[:ingredients]
+                  end 
+                end
+              end 
+          end 
+        end
+    end
     expected = ["Flour", "Eggs", "Milk", "Syrup"]
-    expect(pancake_ingredients).to eq(expected)
+    expect(pancake_ingredients.flatten).to eq(expected)
   end
 
-  xit 'test 3' do
+  it 'test 3' do[
+      "Rice",
+      "Cheese",
+      "Butter",
+      "Beef",
+      "Garlic",
+      "Flour",
+      "Eggs",
+      "Milk",
+      "Syrup",
+      "Flour",
+      "Eggs",
+      "Syrup",
+      "Bun",
+      "Hamburger",
+      "Ketchup",
+      "pickles",
+      "Potatoes",
+      "Salt"
+    ]
     # Find the price of risotto
-    risotto_price = ____
-
+    risotto_price = 0
+    stores.each do |key, value| 
+      value.each do |k, v| 
+       if k == :dishes
+            v.each do |k1, v1| 
+               k1.each do |k2, v2|
+                if v2 == "Risotto"
+                  risotto_price =  k1[:price]
+                end 
+              end 
+            end
+          end
+        end 
+      end
     expect(risotto_price).to eq(12)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     # Find the ingredients for a Big Mac
-    big_mac_ingredients = ____
-
+    big_mac_ingredients = []
+      stores.each do |key, value| 
+        value.each do |k, v| 
+        if k == :dishes
+              v.each do |k1, v1| 
+                k1.each do |k2, v2|
+                  if v2 == "Big Mac"
+                    big_mac_ingredients <<  k1[:ingredients]
+                  end 
+                end 
+              end
+            end
+          end 
+        end
     expected = ['Bun','Hamburger','Ketchup','pickles']
-    expect(big_mac_ingredients).to eq(expected)
+    expect(big_mac_ingredients.flatten).to eq(expected)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     # Find a list of restaurants
-    store_names = ____
-
+    store_names = stores.keys
     expected = [:olive_garden, :dennys, :macdonalds]
     expect(store_names).to eq(expected)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     # Find dishes names for Olive Garden
-    dishes_names = ____
+    dishes_names= []
+     stores.each do |key, value| 
+      if key == :olive_garden
+        value.each do |k, v|
+          if k == :dishes 
+            v.each do |k1, v1|
+              k1.each do |k2, v2|
+              if k2 == :name 
+                dishes_names << v2
+              end
+              end
+            end
+          end
+        end
+      end
+    end
 
     expect(dishes_names).to eq(['Risotto', 'Steak'])
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     # Return a list of employees across
     # all restaurants
-    employee_names = ____
+    employee_names = []
+      stores.each do |key, value| 
+        value.each do |k, v|
+          if k == :employees 
+              employee_names << v 
+          end
+        end
+      end
 
     expected = ["Jeff", "Zach", "Samantha", "Bob", "Sue", "James", "Alvin", "Simon", "Theodore"]
-    expect(employee_names).to eq(expected)
-  end
+    expect(employee_names.flatten).to eq(expected)
+end
 
-  xit 'test 8' do
+  it 'test 8' do
     # Return a list of all ingredients
     # across all restaurants
-    ingredients = ____
+    ingredients = []
+      stores.each do |key, value| 
+        value.each do |k, v|
+          if k == :dishes 
+            v.each do |k1|
+              k1.each do |k2, v2|
+                if k2 == :ingredients 
+                  ingredients << v2 
+                end
+              end
+            end
+          end
+        end
+      end
 
     expected = [
       "Rice",
@@ -93,21 +182,53 @@ RSpec.describe 'Advanced Nested Collections' do
       "Potatoes",
       "Salt"
     ]
-    expect(ingredients).to eq(expected)
+    expect(ingredients.flatten).to eq(expected)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     # Return the full menu price for Olive Garden
-    full_menu_price = ____
+    full_price = []
+    stores.each do |key, value| 
+      if  key == :olive_garden
+        value.each do |k, v| 
+          if k == :dishes
+            v.each do |k1| 
+              k1.each do |k2, v2|
+                if k2 == :price
+                  full_price << v2
+                end
+              end
+            end
+          end
+        end
+      end 
+    end
 
+    sum = 0
+    
+    full_price.each do |l|
+      sum += l 
+    end 
+    
+    full_menu_price = sum
     expect(full_menu_price).to eq(27)
   end
 
-  xit 'test 10' do
+  it 'test 10' do
     # Return the full menu for Olive Garden
 
-    olive_garden_menu = _____
-    
+    hash = {}
+      stores.each do |key, value| 
+        if key == :olive_garden 
+          value.each do |k,v|
+            if k == :dishes
+              hash["Risotto"] = v[0]
+              hash["Steak"] = v[1]
+            end 
+          end
+        end
+      end
+      olive_garden_menu = hash
     expected = {
       "Risotto" => {
         :name => "Risotto",
@@ -123,10 +244,36 @@ RSpec.describe 'Advanced Nested Collections' do
     expect(olive_garden_menu).to eq(expected)
   end
 
-  xit 'test 11' do
+  it 'test 11' do
     # Return a full menu across all restaurants
-    full_menu = ____
-
+    hash = {}
+    stores.each do |key, value| 
+      if key == :olive_garden 
+        value.each do |k,v|
+          if k == :dishes
+            hash["Risotto"] = v[0]
+            hash["Steak"] = v[1]
+          end 
+        end
+      end
+      if key == :dennys 
+        value.each do |k1,v1|
+          if k1 == :dishes
+            hash["Pancakes"] = v1[0]
+            hash["Waffles"] = v1[1]
+          end 
+        end
+      end
+      if key == :macdonalds 
+        value.each do |k2,v2|
+          if k2 == :dishes
+            hash["Big Mac"] = v2[0]
+            hash["Fries"] = v2[1]
+          end 
+        end
+      end    
+    end
+    full_menu = hash
     expected = {
       "Risotto" => {
         :name => "Risotto",
