6c3f27fe65346b4abe5bb595b14d692c5c6d98d6
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-31 19:31:45 +0530

    green: state, by adding class

--- association_oop/bidirectional_association_spec.rb
+++ association_oop/bidirectional_association_spec.rb
@@ -1,8 +1,10 @@
+class Author 
+end
+
 RSpec.describe "Author and book relation" do 
   context "Implementation of classes" do 
     it "can add class Author" do 
       author = Author.new
     end
   end
-
 end
