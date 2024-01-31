97d475bcd4caea6ea43d46527f91db7f53ff06a6
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-31 19:56:32 +0530

    green: states, after refactoring

--- association_oop/bidirectional_association_spec.rb
+++ association_oop/bidirectional_association_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe "Author and book relation" do
 
     it "can add a book in Author class" do 
       author = Author.new
-      expect(author.add_book("Book1")).to eq("Book1")
+      expect(author.add_book("Book1")).to eq(["Book1"])
     end
   end
 end
