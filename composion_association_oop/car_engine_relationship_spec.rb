ce01811d52c6b3fd4b2926c0d7b2667c69d93ac7
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-31 12:13:29 +0530

    test added: red state

---
+++ composion_association_oop/car_engine_relationship_spec.rb
@@ -0,0 +1,16 @@
+=begin
+Composition in a Car-Engine Relationship:
+
+Problem: Define a Car class and an Engine class in Ruby, where a Car is composed of one Engine. 
+The Engine class should have methods like start and stop. When a Car is destroyed, its associated Engine 
+should also be destroyed.
+Coding Task: Implement the Car and Engine classes with the described methods. Show how creating and destroying
+ a Car object will also create and destroy the associated Engine object.
+=end
+
+RSpec.describe "Car and engine relationship" do 
+  it "Implement car and engine class" do 
+    car = Car.new
+    expect(car.start).to eq("car started")
+  end
+end
