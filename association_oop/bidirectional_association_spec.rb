0417041d86102d7e384a5b89d3d7a90cc47c7874
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-01-31 19:36:29 +0530

    red: state , undefine method

--- association_oop/bidirectional_association_spec.rb
+++ association_oop/bidirectional_association_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Author and book relation" do
   context "Implementation of classes" do 
     it "can add class Author" do 
       author = Author.new
+      expect(author.books).to eq([])
     end
   end
 end
